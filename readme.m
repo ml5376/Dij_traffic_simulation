9130b891afbcdd0775b5d8332560c0cb873806b1
ml5376 <55029181+ml5376@users.noreply.github.com>
2024-08-03 23:53:06 +0200

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,8 +1,8 @@
-NYU Undergraduate Course on Modeling and Simulation Research:
+# NYU Undergraduate Course on Modeling and Simulation Research:
 https://modelingsimulation.github.io/undergraduateClass/ 
 
 ![image](https://github.com/ml5376/Dij_traffic_simulation/blob/main/demo1.gif)
 
-prof.Peskin's lecture notes: 
+# prof.Peskin's lecture notes: 
 used and taught in class 
 https://www.math.nyu.edu/~peskin/modsim_lecture_notes/index.html
